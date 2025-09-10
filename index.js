248995f9f9d8f53b383bc72f33e2a67fc73bd644
shagun-ux <shaugntyagityagi@gmail.com>
2025-09-10 06:56:08 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+// Simple news route
+app.get("/news/:category", async (req, res) => {
+  const { category } = req.params;
+  const page = req.query.page || 1;
+  const pageSize = req.query.pageSize || 3;
+
+  const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.key}&page=${page}&pageSize=${pageSize}`;
+
+  try {
+    
+    const response = await axios.get(url);
+
+    res.json(response.data,
+      
+    );
+   
+      
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch API data" });
+  }
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
